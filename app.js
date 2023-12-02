a0616bbf0c99d1088d667592610089271c5428d5
Nikita <personalnick@yandex.ru>
2023-12-02 23:58:14 +0300

    fix id v2

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process
 const app = express();
 app.use(cors());
 
-mongoose.connect('mongod://localhost:27017/mestodb', {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -20,6 +20,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133',
+  };
+  next();
+});
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
