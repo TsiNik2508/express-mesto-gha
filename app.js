1da5c2572dc3a73544ff09322b3fd04625005f5b
Nikita <personalnick@yandex.ru>
2023-12-04 20:03:21 +0300

    1

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const helmet = require("helmet");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const usersRouter = require('./routes/users');
 
-const { PORT = 3000, DB_URL = "mongodb://127.0.0.1:27017/mestodb" } =
-  process.env;
 const app = express();
+const port = 3000;
 
-mongoose.connect(DB_URL).then(() => {
-  console.log("MongoDB is connected");
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useUnifiedTopology: true,
 });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  req.user = {
-    _id: "5d8b8592978f8bd833ca8133",
-  };
 
-  next();
-});
-app.use(helmet());
-app.use("/users", require("./routes/users"));
-app.use("/cards", require("./routes/cards"));
-app.listen(PORT, () => {
-  console.log("Server is connected");
+app.use('/api', usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 
 const {
   getAllUsers, getUserById, createUser, updateProfile, updateAvatar,
