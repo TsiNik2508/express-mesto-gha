a8b97ba93e98b06ea48f493e3516feeba179cc5a
Nikita <personalnick@yandex.ru>
2023-12-05 20:15:27 +0300

    2.5 edit app.js

--- app.js
+++ app.js
@@ -1,51 +1,27 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const { login, createUser } = require('./controllers/users');
 
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
-const port = process.env.PORT || 3000;
 
-const dbURI = process.env.DB_URL || 'mongodb://localhost:27017/mestodb';
-
-mongoose.connect(dbURI, { useUnifiedTopology: true, useNewUrlParser: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('MongoDB connection error:', error);
-    process.exit(1);
-  });
-
-app.use(express.json());
-
-app.use((req, res, next) => {
-  req.user = {
-    _id: '656f53bf3cbcda266185890f',
-  };
-
-  next();
+mongoose.connect(DB_URL).then(() => {
+  console.log('MongoDB is connected');
 });
 
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  const error = new Error('Not Found');
-  error.status = 404;
-  next(error);
+app.post('/signin', login);
+app.post('/signup', createUser);
+
+app.use(helmet());
+
+app.all('*', (req, res) => {
+  res.status(404).send({ message: 'Неверный путь' });
 });
 
-app.use((err, req, res, next) => {
-  res.status(err.status || 500).json({ error: err.message });
-});
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log('Server is connected');
 });
