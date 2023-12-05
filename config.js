3eb5c2568b8457d120e7c7df93af24e833425a68
Nikita <personalnick@yandex.ru>
2023-12-05 19:52:53 +0300

    2.2

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
+
 const config = {
-  DB_URL: 'mongodb://127.0.0.1:27017/mestodb',
+  DB_URL: process.env.DB_URL || 'mongodb://localhost:27017/mestodb',
   mongoURI: `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
 };
+
 module.exports = config;
