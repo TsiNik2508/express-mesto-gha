42bffe642d81e488fe70c8a5b551b4ae03ee318c
Nikita <personalnick@yandex.ru>
2023-12-05 20:01:22 +0300

    2.4 edit app.js

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const dbURI = process.env.DB_URL || 'mongodb://localhost:27017/mestodb';
 
-mongoose.connect(dbURI, { useUnifiedTopology: true })
+mongoose.connect(dbURI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
-    console.log('MongoDB is connected');
+    console.log('Connected to MongoDB');
   })
   .catch((error) => {
     console.error('MongoDB connection error:', error);
+    process.exit(1);
   });
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use((req, res, next) => {
   req.user = {
@@ -26,15 +26,25 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ error: err.message });
+});
+
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
-
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
