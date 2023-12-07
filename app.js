d22a978921d0626aed05b05d6b8ba74f17feefac
Nikita <personalnick@yandex.ru>
2023-12-07 16:13:33 +0300

    2.1.4

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const dbURI = process.env.DB_URL || 'mongodb://localhost:27017/mestodb';
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log('Соединение с MongoDB установлено');
   })
   .catch((error) => {
-    console.error('MongoDB connection error:', error);
+    console.error('Ошибка соединения с MongoDB:', error);
     process.exit(1);
   });
 
@@ -31,13 +31,13 @@ app.use('/cards', require('./routes/cards'));
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', console.error.bind(console, 'Ошибка соединения с MongoDB:'));
 db.once('open', () => {
-  console.log('Connected to MongoDB');
+  console.log('Соединение с MongoDB установлено');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Сервер запущен на http://localhost:${port}`);
 });
 
 app.use((req, res, next) => {
@@ -47,6 +47,10 @@ app.use((req, res, next) => {
   next(error);
 });
 
-app.use('*', (req, res) => {
-  res.status(http2.constants.HTTP_STATUS_NOT_FOUND).send({ message: 'Неверный путь' });
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  error.message = 'Endpoint not found';
+
+  res.status(http2.constants.HTTP_STATUS_NOT_FOUND).json({ message: 'Неверный путь' });
 });
