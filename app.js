4378871135895721d1e2e8d7d7fc264c7d545b7f
Nikita <personalnick@yandex.ru>
2023-12-07 15:47:52 +0300

    2.8

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const http2 = require('http2');
-const routes = require('./routes');
 
 const app = express();
+const port = process.env.PORT || 3000;
+
+const dbURI = process.env.DB_URL || 'mongodb://localhost:27017/mestodb';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  });
 
 app.use(express.json());
-
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
-
-mongoose.connect(DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
 app.use((req, res, next) => {
   req.user = {
@@ -22,10 +25,26 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(routes);
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
-app.use('*', (req, res) => {
-  res.status(http2.constants.HTTP_STATUS_NOT_FOUND).send({ message: 'Неверный путь' });
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
 });
 
-app.listen(PORT);
+app.use((err, req, res) => {
+  res.status(err.status || 500).json({ error: { message: err.message } });
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
