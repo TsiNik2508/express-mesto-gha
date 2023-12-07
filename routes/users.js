0140f458d9001a344da54408c98fa15387be7730
Nikita <personalnick@yandex.ru>
2023-12-07 15:58:06 +0300

    2.9

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
-
 const {
-  getAllUsers, getUserById, createUser, updateProfile, updateAvatar,
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateProfile,
+  updateAvatar,
 } = require('../controllers/users');
 
-router.post('/users', createUser);
-router.get('/users', getAllUsers);
-router.get('/users/:userId', getUserById);
-router.patch('/users/me', updateProfile);
-router.patch('/users/me/avatar', updateAvatar);
+router.get('/', getAllUsers);
+router.get('/:userId', getUserById);
+router.post('/', createUser);
+router.patch('/me', updateProfile);
+router.patch('/me/avatar', updateAvatar);
 
 module.exports = router;
