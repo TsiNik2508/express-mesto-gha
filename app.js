0fecbd003eda5647f6f713f9802f2420263c64a4
Nikita <personalnick@yandex.ru>
2023-12-10 19:31:04 +0300

    refactoring 1.0.2

--- app.js
+++ app.js
@@ -9,38 +9,42 @@ const router = require('./routes/route');
 
 const app = express();
 
-const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+app.use(bodyParser.json());
+const { validateSignup, validateSignin } = require('./middlewares/validate');
 
+const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+const { createUser, login } = require('./controllers/auth');
+
+app.post('/signin', validateSignin, login);
+app.post('/signup', validateSignup, createUser);
+app.use(auth);
+app.use(router);
 app.use(helmet());
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
 });
 app.use(limiter);
-app.use(bodyParser.json());
-app.use(auth);
-app.use(router);
-
 async function connect() {
   try {
     await mongoose.set('strictQuery', false);
-    await mongoose.connect(MONGO_URL);
-    console.log(`App connected to ${MONGO_URL}`);
+    await mongoose.connect('mongodb://localhost:27017/mestodb');
+    console.log(`App connected ${MONGO_URL}`);
     await app.listen(PORT);
     console.log(`App listening on port ${PORT}`);
   } catch (err) {
     console.error(err);
   }
 }
-
-connect();
-
 app.use(errors());
-
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message = 'На сервере произошла ошибка' } = err;
-  res.status(statusCode).send({ message });
-  return next();
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500
+      ? 'На сервере произошла ошибка'
+      : message,
+  });
+  next();
 });
 
-module.exports = app;
+connect();
