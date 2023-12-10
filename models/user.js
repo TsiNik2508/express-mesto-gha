b87847e091c1b125c34e04a4df5da71ab3a1fb81
Nikita <personalnick@yandex.ru>
2023-12-10 19:07:10 +0300

    refactoring 1.7

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const validator = require('validator');
+const validateUrl = require('validator/lib/isURL');
+const validateEmail = require('validator/lib/isEmail');
 const Unauthorized = require('../error/Unauthorized');
 
 const userSchema = new mongoose.Schema({
@@ -18,8 +19,9 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
+    default: 'https://cdn.forbes.ru/forbes-static/c/908x511/new/2022/05/rick-and-morty-siemki-3-62851a0fad390.webp',
     validate: {
-      validator: (url) => validator.isURL(url),
+      validator: validateUrl,
       message: 'Некорректный адрес URL',
     },
   },
@@ -28,7 +30,7 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
     validate: {
-      validator: (email) => validator.isEmail(email),
+      validator: validateEmail,
       message: 'Некорректный адрес почты',
     },
   },
@@ -43,12 +45,12 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Unauthorized('Неправильный адрес почты или пароль'));
+        return Promise.reject(new Unauthorized('Пользователь с таким адресом почты не найден'));
       }
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Unauthorized('Неправильный адрес почты или пароль'));
+            return Promise.reject(new Unauthorized('Неправильный пароль'));
           }
           return user;
         });
