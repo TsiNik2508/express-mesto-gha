3d78bff4052e518696574f2d86556963ea2739a3
Nikita <personalnick@yandex.ru>
2023-12-11 18:15:56 +0300

    refactoring 1.1.0

--- controllers/card.js
+++ controllers/card.js
@@ -43,11 +43,11 @@ module.exports.likeCard = (req, res, next) => {
     });
 };
 
-module.exports.likeCard = (req, res, next) => {
+module.exports.dislikeCard = (req, res, next) => {
   cardSchema
     .findByIdAndUpdate(
       req.params.cardId,
-      { $addToSet: { likes: req.user._id } },
+      { $pull: { likes: req.user._id } },
       { new: true },
     )
     .then((card) => {
