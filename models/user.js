1019a52a8b74cfb0e165d91bdbf4eda81defeeb2
Nikita <personalnick@yandex.ru>
2023-12-11 17:31:53 +0300

    refactoring 1.0.7

--- models/user.js
+++ models/user.js
@@ -9,17 +9,20 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
     validate: {
       validator: validateUrl,
       message: 'Некорректный адрес URL',
+      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     },
   },
   email: {
